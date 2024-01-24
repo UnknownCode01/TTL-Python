319ccff9f8797dd7478867616344ff09a372c4c2
Brijit Adak <104793623+UnknownCode01@users.noreply.github.com>
2024-01-25 00:01:25 +0530

    Add files via upload

---
+++ q8.py
@@ -0,0 +1,6 @@
+from math import * 
+print("Hello roll 21052410, Enter the radius of a circle to print its area and perimeter")
+r=eval(input("r="))
+a=pi*r*r
+p=2*pi*r
+print("Area = ",a," Perimeter = ",p)
