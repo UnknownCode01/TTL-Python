726f6a18a11be9e22914cbaf2b5e70b3821066b8
Brijit Adak <104793623+UnknownCode01@users.noreply.github.com>
2024-01-26 20:00:44 +0530

    Delete q4.py

--- q4.py
+++
@@ -1,8 +0,0 @@
-print("Hello roll 21052410, Enter the the basic salary of a person to compute its TA (20% of basic), DA (120% of basic), HRA (30% of basic), Gross (basic + ta + da + hra)")
-b=eval(input("Enter the basic salary "))
-ta=.2*b
-da=1.2*b
-hra=.3*b
-gross=b+ta+da+hra
-print("TA=",ta," DA=",da," HRA=",hra," Gross=",gross)
-
