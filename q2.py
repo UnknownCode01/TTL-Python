dc9cdc7b0d04ca580aad222cf3aef425a7f0842c
Brijit Adak <104793623+UnknownCode01@users.noreply.github.com>
2024-01-26 19:59:46 +0530

    Delete q2.py

--- q2.py
+++
@@ -1,7 +0,0 @@
-print("Hello roll 21052410, Enter values of 2 numbers a and b to swap their values without using 3rd variable")
-a=eval(input("Enter a "))
-b=eval(input("Enter b "))
-a=a+b
-b=a-b
-a=a-b
-print("Now a=",a," b=",b)
